2c5eddaa824989e6d6e3f67ace80302fd39f94d8
tomcat <qtomcat@mail.ru>
2015-09-14 08:55:29 +0000

    trial

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var express = require('express')
 var app = express()
 var buffer = new Buffer(16);
 var fs = require ('fs');
-
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
