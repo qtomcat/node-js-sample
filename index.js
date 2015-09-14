cfc197a994a482bea67033988700c6e812683680
tomcat <qtomcat@mail.ru>
2015-09-14 07:39:33 +0000

    new file

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Worldw2')
+  response.send('Hello Worldw2');
+  response.sendfile('index.html');
   fs.readFile('index.html', 'utf8', function(err, data) {
      if (err) {
          return console.log(err);
