a5acdafe3e0b34d6deae9b735d3f44d81a54f183
qtomcat <ubuntu@ip-172-31-47-226.us-west-2.compute.internal>
2015-09-20 08:02:23 +0000

    bug fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 var buffer = new Buffer(16);
 var fs = require ('fs');
-app.set('port', (process.env.PORT || 8080))
+app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 app.get('/', function(request, response) {
   
